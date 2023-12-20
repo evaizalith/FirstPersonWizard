768a112159732200186ed39c3da4b82411c4f4e2
WaddlesPF <113570007+WaddlesPF@users.noreply.github.com>
2023-12-19 22:38:05 -0600

    Add enemy

---
+++ scripts/enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+var player
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	player = $"../../Player/CharacterBody3D"
+	
+func _process(delta):
+	var distToPlayer = global_position.distance_to(player.transform.origin)
+	print()
+	if distToPlayer > 2.5:
+		velocity = global_position.direction_to(player.transform.origin)
+	else:
+		velocity = Vector3(0,velocity.y,0)
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	move_and_slide()
